73d56e72ddfd56fd5a92d7f11123231fb3599559
Paul Julian <bpj101@dameagnesproductions.com>
2015-08-30 01:08:29 -0400

    Initial commit set up Jobs Directory

---
+++ app/models/job.server.model.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Job Schema
+ */
+var JobSchema = new Schema({
+  company: {
+    type: String,
+    default: '',
+    required: 'Please fill in Company',
+    trim: true
+  },
+  title: {
+    type: String,
+    default: '',
+    required: 'Please fill Job title',
+    trim: true
+  },
+  description: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  requirements: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  hourly_wage: {
+    type: Number,
+    default: '',
+    trim: true
+  },
+  state: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
+});
+
+mongoose.model('Job', JobSchema);
